f492830e1bc6f2f25e783f8defcfd019f6053699
Roman Rastiehaiev <roman.rastiehaiev@gmail.com>
2020-06-14 20:15:34 +0300

    Add logging.

--- src/main/kotlin/com/sbrati/spring/boot/starter/kotlin/telegram/gcp/pubsub/GcpPubSubTelegramSubscriber.kt
+++ src/main/kotlin/com/sbrati/spring/boot/starter/kotlin/telegram/gcp/pubsub/GcpPubSubTelegramSubscriber.kt
@@ -3,9 +3,16 @@ package com.sbrati.spring.boot.starter.kotlin.telegram.gcp.pubsub
 import com.sbrati.spring.boot.starter.gcp.pubsub.GcpPubSubSubscriber
 import com.sbrati.spring.boot.starter.kotlin.telegram.manager.TelegramManager
 import com.sbrati.telegram.domain.Event
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
+import kotlin.properties.ReadOnlyProperty
+import kotlin.reflect.KProperty
+import kotlin.reflect.full.companionObject
 
 open class GcpPubSubTelegramSubscriber<T>(type: Class<T>, topicName: String) : GcpPubSubSubscriber<T>(type, topicName) {
+
+    private val log by LoggerDelegate()
 
     @Autowired
     private lateinit var telegramManager: TelegramManager
@@ -13,6 +20,17 @@ open class GcpPubSubTelegramSubscriber<T>(type: Class<T>, topicName: String) : G
     override fun process(event: Event<T>?) {
         if (event != null) {
             telegramManager.onEvent(event)
+        } else {
+            log.warn("Received an empty event.")
         }
     }
+}
+
+private class LoggerDelegate<in R : Any> : ReadOnlyProperty<R, Logger> {
+    override fun getValue(thisRef: R, property: KProperty<*>): Logger =
+        LoggerFactory.getLogger(getClassForLogging(thisRef.javaClass))
+}
+
+private fun <T : Any> getClassForLogging(javaClass: Class<T>): Class<*> {
+    return javaClass.enclosingClass?.takeIf { it.kotlin.companionObject?.java == javaClass } ?: javaClass
 }
